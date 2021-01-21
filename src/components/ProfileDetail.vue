2a12f0b0a777527a96eee726d6a68129df2b8b18
Aef Badmus <realafe21@gmail.com>
2021-01-21 13:06:15 +0100

    first commit

---
+++ src/components/ProfileDetail.vue
@@ -0,0 +1,63 @@
+<template>
+  <div class="profile">
+    <p>
+      <span>Name</span>: {{ profile.FirstName }} {{ profile.FirstName }}</p>
+    <p>
+      <span>Payment Method</span>: {{ profile.PaymentMethod }}</p>
+    <p>
+      <span>Gender</span>: {{ profile.Gender }}</p>
+    <p>
+      <span>CreditCard Type</span>: {{ profile.CreditCardType }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Profile',
+  props: {
+    profile: {
+      type: Object,
+      default: function () {
+        return {
+          FirstName: "Kailee",
+          LastName: "Schaefer",
+          Gender: "Male",
+          Latitude: -36.518723,
+          Longitude: -112.08739,
+          CreditCardNumber: "348225436399406",
+          CreditCardType: "American Express",
+          Email: "LIuBTpy@QgPWSAQ.org",
+          DomainName: "wdtSvtC.biz",
+          PhoneNumber: "457-391-6210",
+          MacAddress: "88:66:af:1c:ec:51",
+          URL: "https://ykCGbJW.com/",
+          UserName: "UsWemsp",
+          LastLogin: "2011-02-26 17:29:53",
+          PaymentMethod: "check",
+        }
+      }
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+.profile {
+  display: flex;
+  flex-wrap: wrap;
+  width: 40%;
+  background: linear-gradient(275.47deg, #8C51A1 2.71%, #360F51 99.61%);
+  border-radius: 25px;
+  padding: 10px 20px 20px;
+  margin-top: 20px;
+  span {
+    font-weight: 600;
+  }
+  p {
+    margin-right: 20px;
+    margin-top: 10px;
+    color: #fafafa;
+  }
+}
+</style>
